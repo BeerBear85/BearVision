bd07fffcfb2a8e6a07f7d6da4c6d8393ab8fb873
BeerBear85 <bear.eskildsen@gmail.com>
2018-05-14 21:45:48 +0200

    aaa

---
+++ test/run_test.py
@@ -0,0 +1,13 @@
+import sys,os
+
+sys.path.append('..\code\Modules')
+sys.path.append('..\code\Application')
+import Application
+
+tmp_video_folder = os.path.abspath("input_video")
+tmp_user_folder  = os.path.abspath("users")
+
+print("Starting test!\n")
+
+app_instance = Application.Application()
+app_instance.run(tmp_video_folder, tmp_user_folder)
