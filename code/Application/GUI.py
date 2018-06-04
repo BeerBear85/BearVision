d67c5471ff91f49408257ed3f0e4e999593ad741
BeerBear85 <bear.eskildsen@gmail.com>
2018-06-04 22:06:02 +0200

    GUI is at a fine state. The dialogs are present and working. But is not integrated into the application yet

--- code/Application/GUI.py
+++ code/Application/GUI.py
@@ -1,13 +1,10 @@
 from tkinter import *
 from tkinter import filedialog
 from tkinter import font
+import os
 
 pa_default_input_video_folder = "input_video"
 pa_default_user_folder = "users"
-
-#big_font = font.Font(family='Helvetica', size=36, weight='bold')
-#appHighlightFont = font.Font(family='Helvetica', size=12, weight='bold')
-#font.families()
 
 class MyFirstGUI:
     def __init__(self, master):
@@ -15,42 +12,38 @@ class MyFirstGUI:
         master.title("BearVision WakeVision")
         master.geometry("500x500")
 
-        self.welcome_label = Label(master, text="Welcome to WakeVison")
-        self.welcome_label.pack(side=TOP)
+        self.welcome_label = Label(master, text="BearVision - WakeVison", bg='red', font=('Helvetica', '20'))
+        self.welcome_label.pack(fill=X, side=TOP)
 
-        self.video_folder_frame = Frame(master)
-        self.video_folder_frame.pack()
+        self.folder_selection_frame = Frame(master)
+        self.folder_selection_frame.pack(fill=X, pady=10, side=TOP)
+        self.folder_selection_frame.columnconfigure(0, weight=3)
+        self.folder_selection_frame.columnconfigure(1, weight=1)
+
         self.video_folder_text = StringVar()
-        self.video_folder_text.set(pa_default_input_video_folder)
-        self.video_folder_label = Label(self.video_folder_frame, textvariable=self.video_folder_text)
-        self.video_folder_label.pack(side=LEFT)
-        self.video_folder_button = Button(self.video_folder_frame, text="Select input video folder", command=self.set_input_video_folder)
-        self.video_folder_button.pack(side=LEFT)
+        self.video_folder_text.set( os.path.abspath(pa_default_input_video_folder) )
+        self.video_folder_entry = Entry(self.folder_selection_frame, textvariable=self.video_folder_text)
+        self.video_folder_entry.grid(row=0, column=0, sticky=W+E)
+        self.video_folder_button = Button(self.folder_selection_frame, text="Select input video folder", command=self.set_input_video_folder)
+        self.video_folder_button.grid(row=0, column=1, sticky=W+E)
 
-        self.user_folder_frame = Frame(master)
-        self.user_folder_frame.pack()
         self.user_folder_text = StringVar()
-        self.user_folder_text.set(pa_default_user_folder)
-        self.user_folder_label = Label(self.user_folder_frame, textvariable=self.user_folder_text)
-        self.user_folder_label.pack( side=LEFT )
-        self.user_folder_button = Button(self.user_folder_frame, text="Select user base folder", command=self.set_user_folder)
-        self.user_folder_button.pack( side=LEFT )
-
-        #self.check_var_1 = IntVar()
-        #self.checkbox_1 = Checkbutton(master, text="Option 1", variable = self.check_var_1)
-        #self.checkbox_1.pack()
+        self.user_folder_text.set( os.path.abspath(pa_default_user_folder) )
+        self.user_folder_entry = Entry(self.folder_selection_frame, textvariable=self.user_folder_text, width=60)
+        self.user_folder_entry.grid(row=1, column=0, sticky=W+E)
+        self.user_folder_button = Button(self.folder_selection_frame, text="Select user base folder", command=self.set_user_folder)
+        self.user_folder_button.grid(row=1, column=1, sticky=W+E)
 
         self.run_options = Listbox(master, selectmode=MULTIPLE )
-        self.run_options.insert(1, "Generate motion start files")
-        self.run_options.insert(2, "Initialize users")
-        self.run_options.insert(3, "Match user locations to motion files")
-        self.run_options.insert(4, "Generate output videos")
-        self.run_options.pack()
-        #self.run_options.activate(1)
-        #self.run_options.activate(self.run_options.index(3))
+        self.run_options.insert(END, "Generate motion start files")
+        self.run_options.insert(END, "Initialize users")
+        self.run_options.insert(END, "Match user locations to motion files")
+        self.run_options.insert(END, "Generate output videos")
+        self.run_options.pack(fill=X, pady=10)
+        self.run_options.selection_set(0,self.run_options.size()) #select all
 
-        self.run_button = Button(master, text="Run", command=self.run)
-        self.run_button.pack()
+        self.run_button = Button(master, text="Run", command=self.run, bg='green3', height = 1, width = 10, font=('Helvetica', '20'))
+        self.run_button.pack(pady=10)
 
     def set_input_video_folder(self):
         selected_directory = filedialog.askdirectory()
@@ -64,7 +57,7 @@ class MyFirstGUI:
 
     def run(self):
         print("Running!")
-        print("Running checkbox_1: " + str(self.check_var_1.get()))
+        print("Running selections: " + str(self.run_options.curselection() ))
 
 
 
