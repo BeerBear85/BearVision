1ae5dc871c6d36e3b47ca3e32c3a3a571a15a668
BeerBear <34023565+BeerBear85@users.noreply.github.com>
2025-07-30 09:17:58 +0200

    Add event-based HindSight trigger

--- tests/test_gopro_controller.py
+++ tests/test_gopro_controller.py
@@ -7,6 +7,7 @@ sys.path.append(str(MODULE_DIR))
 from GoProController import GoProController
 
 from tests.stubs.gopro import FakeGoPro
+from open_gopro.models.constants import constants
 
 
 def test_list_and_download(tmp_path):
@@ -29,3 +30,12 @@ def test_configure_and_preview():
         assert gopro.http_settings.hindsight.value is not None
         url = ctrl.start_preview(9000)
         assert url == 'udp://127.0.0.1:9000'
+
+
+def test_start_hindsight_clip():
+    with mock.patch('GoProController.WirelessGoPro', FakeGoPro):
+        ctrl = GoProController()
+        ctrl.start_hindsight_clip(0)
+        gopro = ctrl._gopro
+        assert gopro.http_command.shutter == [constants.Toggle.ENABLE, constants.Toggle.DISABLE]
+
